a599082c37a524c206cadb0bfc2655fafb99ad7d
Fa19959 <fa6amyalmamari@gmail.com>
2023-11-26 11:00:35 +0400

    Added comment to the code

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace MyAwesomeProgram
 {
     class Program
     {
+        //Entry Method 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.ReadKey();// This line waits for the user to press button, so that the window will not ending or closing 
         }
     }
 }
