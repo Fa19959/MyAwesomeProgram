b926218d6d32b54f4952a44b62aa56015b0a8d7c
Fa19959 <fa6amyalmamari@gmail.com>
2023-11-26 14:34:24 +0400

    Find the average of 3 numbers

--- Program.cs
+++ Program.cs
@@ -23,7 +23,21 @@ namespace MyAwesomeProgram
             num02 = Convert.ToInt32(Console.ReadLine());
             int result = num01 * num02;
             Console.WriteLine("The result: "+ result);
-            
+            //Find the avrage of 3 numbers
+            int number;
+            int number2;
+            int number3;
+            Console.Write("Enter the first number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            number3 = Convert.ToInt32(Console.ReadLine());
+            int avrage =(number +number2 + number3)/3;
+            Console.WriteLine("The avrage: "+ avrage);
+
+
+
             Console.ReadKey();// "Wait before close" This line waits for the user to press button, so that the window will not ending or closing 
         }
     }
