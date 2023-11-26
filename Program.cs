7ad3301e5fbdf1e8ee15d4e40edbbebdc49fa956
Fa19959 <fa6amyalmamari@gmail.com>
2023-11-26 14:19:26 +0400

    Added 2 numbers and get the result

--- Program.cs
+++ Program.cs
@@ -13,6 +13,17 @@ namespace MyAwesomeProgram
             Console.WriteLine("What is your name?");
             String userName = Console.ReadLine();
             Console.WriteLine("Hello " + userName + ",nice to meet you");
+
+            //Adding 2 numbers
+            int num01;
+            int num02;
+            Console.Write("Enter the first number: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+            int result = num01 * num02;
+            Console.WriteLine("The result: "+ result);
+            
             Console.ReadKey();// "Wait before close" This line waits for the user to press button, so that the window will not ending or closing 
         }
     }
