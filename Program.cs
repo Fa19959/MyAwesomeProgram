57585c865f59f23d6f1cc66d5934349d552440ae
Fa19959 <fa6amyalmamari@gmail.com>
2023-12-06 11:32:46 +0400

    Added for loop

--- Program.cs
+++ Program.cs
@@ -110,6 +110,10 @@ namespace MyAwesomeProgram
             */
 
             string [] colors = {"red","green","blue"};
+            for (int i = 0; i < 10; i++)
+            {
+                
+            }
         }
     }
 }
