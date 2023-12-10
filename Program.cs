ff008fe86b0f8380a1d7e2e536b97f1183f2dc3d
Fa19959 <fa6amyalmamari@gmail.com>
2023-12-10 11:29:55 +0400

    Added array

--- Program.cs
+++ Program.cs
@@ -112,7 +112,7 @@ namespace MyAwesomeProgram
             string [] colors = {"red","green","blue"};
             for (int i = 0; i < 10; i++)
             {
-                
+               Console.WriteLine(i); 
             }
         }
     }
